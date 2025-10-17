b100166bd959401e95e6815910a94a9b2d430b2b
augusto-rp <afrodriguez@uc.cl>
2025-10-17 13:52:26 -0300

    archivo r

--- tarea1_kardashians/codigo_tarea1.R
+++ tarea1_kardashians/codigo_tarea1.R
@@ -1,5 +1,8 @@
 #Vamos a analizar las emociones presentes en el capitulo final de la primera temporada de The Kardashians
 #Para ello necesitamos abrir el siguiente paquete
  library(text2emotion)
- library(usethis)
-use_git() 
+ library(readr) #para leer txt
+texto<-read_file("tarea1_kardashians/1_10.txt")
+
+#Sin embargo, el texto esta lleno de simbolos que hay que eliminar. Por lo tanto vamos a arreglarlo un poco
+  
