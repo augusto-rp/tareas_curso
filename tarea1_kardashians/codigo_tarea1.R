8eb31b964c862e693203371bb88f83f270a63fa7
augusto-rp <afrodriguez@uc.cl>
2025-10-17 13:43:56 -0300

    Initial commit

--- tarea1_kardashians/codigo_tarea1.R
+++ tarea1_kardashians/codigo_tarea1.R
@@ -1,4 +1,5 @@
 #Vamos a analizar las emociones presentes en el capitulo final de la primera temporada de The Kardashians
 #Para ello necesitamos abrir el siguiente paquete
  library(text2emotion)
- 
+ library(usethis)
+use_git() 
